1466c906d9439ba11e33de1c800448884458c9c9
Amit-34 <Amitramsoni111@gmail.com>
2024-10-20 02:06:19 +0530

    payment method added markup and css

--- src/pages/paymentMethod/index.js
+++ src/pages/paymentMethod/index.js
@@ -1,7 +1,63 @@
 import React from "react";
 
 const PaymentMethod = () => {
-  return <div>Payment Method</div>;
+  return <>
+   <div
+        className="Site-content"
+        style={{
+          backgroundImage:
+            "url('https://fanizm-prod.s3.amazonaws.com/upload/front_bg.png')",
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+<div className="routing-550">
+<div className="web-body-container pb0">
+  <div className="header quiz-header">
+  <div className="head-col-left"></div>
+  <div className="head-col-center"><span className="head-title">Select Payment Method</span></div>
+  <div className="head-col-right">
+  <div className=""></div>
+   </div>
+
+</div>
+
+<div className="default-header-placeholder"></div>
+<div className="burger-menu-backdrop "></div>
+
+<div className="mobile-wrap">
+<div className="web-container web-container-fixed trans-web-container pay-method-wrap"> 
+<div className="row">
+  <div className="col-sm-12">
+    <div className="payable-amt">To Pay<div className="payable-amt-view">
+  <i className="font-style-normal">₹</i><span>155</span></div></div></div></div>
+
+  <div className="how-would-you-view"></div>
+
+  <div class="payment-method-container">
+    <div class="payment-card-row">
+      <div class="payment-card  payment-card-cursor">
+        <div class="c-image">
+          <img alt="" class="img-fluid" src="https://fanizm-prod.s3.amazonaws.com/upload/paymentgetway/1717995284.png"/>
+          </div>
+          <div class="payment-content">
+            <div class="payments-name">Razorpay  </div>
+          <div class="payements-desc">Seamless payments for you</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+  </>;
 };
 
 export default PaymentMethod;
