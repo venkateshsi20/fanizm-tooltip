f5b3a189ad9cabddc0c34d4814a0fab8fe9a6e7b
aakashnegi97 <61917637+aakashnegi97@users.noreply.github.com>
2024-10-22 13:54:05 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
 import React, { lazy, Suspense } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createMemoryRouter,
+  RouterProvider,
+} from "react-router-dom";
 import LiveQuiz from "../components/livequiz";
 
 const AppLayout = lazy(() =>
   import(/* webpackChunkName: "AppLayout" */ "../components/applayout")
 );
 const Home = lazy(() => import(/* webpackChunkName: "home" */ "../pages/home"));
-const AddFund = lazy(() => import(/* webpackChunkName: "home" */ "../pages/addFund"));
-const PaymentMethod = lazy(() => import(/* webpackChunkName: "home" */ "../pages/paymentMethod"));
-const ViewContest = lazy(() => import(/* webpackChunkName: "home" */ "../pages/viewContest"));
+const AddFund = lazy(() =>
+  import(/* webpackChunkName: "home" */ "../pages/addFund")
+);
+const PaymentMethod = lazy(() =>
+  import(/* webpackChunkName: "home" */ "../pages/paymentMethod")
+);
+const ViewContest = lazy(() =>
+  import(/* webpackChunkName: "home" */ "../pages/viewContest")
+);
 
 const childRoutes = [
   {
@@ -70,7 +80,7 @@ const route = [
   },
 ];
 
-export const router = createBrowserRouter(route);
+export const router = createMemoryRouter(route);
 
 const Routes = () => (
   <Suspense fallback={"loading...."}>
